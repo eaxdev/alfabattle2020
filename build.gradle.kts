281c38aa55908f7dd215d2854a415956bd7040b2
eaxdev <eaxdev@ya.ru>
2020-06-30 10:49:31 +0300

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "io.github.eaxdev"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "alfabattle2020"
+include("task1", "task2", "task3", "task4", "task5")
